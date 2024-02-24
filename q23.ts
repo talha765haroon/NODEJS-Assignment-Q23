1dc9505b72570e82ecd3c3751c1f4613ea6b9f0c
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 04:20:45 +0500

    Add files via upload

---
+++ q23.ts
@@ -0,0 +1,43 @@
+  /*    Ques # 23:
+
+ Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+       
+ let car: string = 'sabaru';
+//console.log("Is car == 'sabaru' ? I predict true otherwise I predict false.")//
+console.log(car === 'sabaru')
+console.log( car === 'civic')
+
+
+let name0: string = 'Sarwat';
+//console.log("Its name0 == 'Sarwat' ? I predict true otherwise I predict false.")//
+console.log(name0 === 'Sarwat')
+console.log(name0 === 'Dania')
+
+
+let age: number = 18;
+//console.log(Its age == 23 ? I predict true other wise I predict false.")//
+console.log(age === 18)
+console.log(age === 20)
+
+
+let food: string = 'Biryani'
+//console.log("Its food == 'Biryani' ? I predict true other I predict false.")//
+console.log(food === 'Biryani')
+console.log(food === 'Pizza')
+
+
+let city: string = 'Karachi'
+//console.log(Its city == 'Pakistan' ? I predict true otherwise I predict false.")//
+console.log(city === 'Karachi')
+console.log(city === 'Lahore')
